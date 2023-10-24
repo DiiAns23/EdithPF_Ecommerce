67b6f807e68c417fc2b4fe6d6076698f59d9cfb9
Diego Andrés Obín Rosales <2742934200101@ingenieria.usac.edu.gt>
2023-10-23 23:19:10 -0600

    ft: agregue la vista carrito desde el usuario

--- src/main/java/com/PF/apirest/controladores/homeController.java
+++ src/main/java/com/PF/apirest/controladores/homeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.PF.apirest.modelo.producto;
@@ -40,6 +41,11 @@ public class homeController {
 
         model.addAttribute("producto", producto);
         return "usuario/productohome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart() {
+        return "usuario/carrito";
     }
     
 }
