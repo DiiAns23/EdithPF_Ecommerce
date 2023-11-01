d8d4045672bc1b60c58d0dd7dfcd122b35102679
Diego Andrés Obín Rosales <2742934200101@ingenieria.usac.edu.gt>
2023-10-31 18:47:11 -0600

    ft: Se busco la cantidad y producto por medio de consola en homeControler para el carrito

--- src/main/java/com/PF/apirest/controladores/homeController.java
+++ src/main/java/com/PF/apirest/controladores/homeController.java
@@ -1,7 +1,9 @@
 package com.PF.apirest.controladores;
 
+import java.util.ArrayList;
 import java.util.Optional;
-
+import java.util.List;
+import com.PF.apirest.modelo.orden;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.PF.apirest.modelo.detalleOrden;
 import com.PF.apirest.modelo.producto;
 import com.PF.apirest.servicios.productoService;
 import org.springframework.ui.Model;
@@ -23,6 +27,12 @@ public class homeController {
 
     @Autowired
     private productoService productoService;
+
+    //para almacenar los detalles de la orden
+    List<detalleOrden> detalles = new ArrayList<detalleOrden>();
+
+    //datos de la orden
+    orden orden = new orden();
 
     @GetMapping("")
     public String home(Model model) {
@@ -44,7 +54,14 @@ public class homeController {
     }
 
     @PostMapping("/cart")
-    public String addCart() {
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+        detalleOrden detalleOrden = new detalleOrden();
+        producto producto = new producto();
+        double sumaTotal = 0;
+
+        Optional<producto> optionalProducto = productoService.get(id);
+        log.info("Producto añadido: {}", optionalProducto.get());
+        log.info("Cantidad: {}", cantidad);
         return "usuario/carrito";
     }
     
